ff211cb5c3f0c5efa5d64420509598809a43bc5b
Valdir <pedrvaldir@gmail.com>
2023-04-17 21:45:59 -0300

    add comentario

--- src/main/java/com/doua/domain/localizacao/Localizacao.java
+++ src/main/java/com/doua/domain/localizacao/Localizacao.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
 public class Localizacao {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 	@Column(name = "latitude")
